4dc9ac937c57e4b1af36d76ccc0a3683c64e6b40
misalnyadev <misalny@gmail.com>
2026-09-18 02:31:54 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Service Worker AiHana — bikin aplikasi bisa dibuka offline & di-"install" */
-const CACHE = 'aihana-v22';
+const CACHE = 'aihana-v23';
 const ASSETS = [
   './',
   './index.html',

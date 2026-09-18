a2de4f62d48f2ff46129763a1540d519a4b4be49
Hana <misalny@gmail.com>
2026-09-18 21:36:42 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Service Worker AiHana — bikin aplikasi bisa dibuka offline & di-"install" */
-const CACHE = 'aihana-v48';
+const CACHE = 'aihana-v49';
 const ASSETS = [
   './',
   './index.html',
